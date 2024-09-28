eaa4f355888653e36f9a37a7e7a3f5bcc2c5e71b
Saurabh Maurya <118221144+saurabhmaurya1309@users.noreply.github.com>
2024-09-28 11:07:13 +0530

    Update application.route.js

--- backend/routes/application.route.js
+++ backend/routes/application.route.js
@@ -4,10 +4,10 @@ import { applyJob, getApplicants, getAppliedJobs, updateStatus } from "../contro
  
 const router = express.Router();
 
-router.route("/apply/:id").get(isAuthenticated, applyJob);
-router.route("/get").get(isAuthenticated, getAppliedJobs);
-router.route("/:id/applicants").get(isAuthenticated, getApplicants);
-router.route("/status/:id/update").post(isAuthenticated, updateStatus);
+router.route("/apply/:id").get( applyJob);
+router.route("/get").get(getAppliedJobs);
+router.route("/:id/applicants").get(getApplicants);
+router.route("/status/:id/update").post(updateStatus);
  
 
 export default router;
