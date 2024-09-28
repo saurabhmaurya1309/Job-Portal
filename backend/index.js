9a7681aef18881e763e61f56258b41d247bdca97
Saurabh Maurya <saurabhmaurya1309@gmail.com>
2024-09-29 05:24:32 +0530

    Updated CORS and cookie-parser configuration

--- backend/index.js
+++ backend/index.js
@@ -16,12 +16,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
-}
-
-app.use(cors(corsOptions));
+app.use(
+	cors({
+		// origin: "http://localhost:5173",
+		origin:'https://job-portal-one-lemon.vercel.app',
+		credentials: true,
+	})
+);
 
 const PORT = process.env.PORT || 3000;
 
--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,3 +1,7 @@
+// export const USER_API_END_POINT="http://localhost:4000/api/v1/user";
+// export const JOB_API_END_POINT="http://localhost:4000/api/v1/job";
+// export const APPLICATION_API_END_POINT="http://localhost:4000/api/v1/application";
+// export const COMPANY_API_END_POINT="http://localhost:4000/api/v1/company";
 export const USER_API_END_POINT="https://job-portal-498o.onrender.com/api/v1/user";
 export const JOB_API_END_POINT="https://job-portal-498o.onrender.com/api/v1/job";
 export const APPLICATION_API_END_POINT="https://job-portal-498o.onrender.com/api/v1/application";
