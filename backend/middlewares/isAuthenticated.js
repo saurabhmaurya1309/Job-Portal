b35b4e0f4b374b1ff2a0f362dea6e83a67f27e9e
Saurabh Maurya <saurabhmaurya1309@gmail.com>
2024-09-29 06:11:05 +0530

    some changes

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -3,6 +3,7 @@ import jwt from "jsonwebtoken";
 const isAuthenticated = async (req, res, next) => {
     try {
         const token = req.cookies.token;
+        console.log("Received token:", token); 
         if (!token) {
             return res.status(401).json({
                 message: "User not authenticated",
@@ -10,6 +11,7 @@ const isAuthenticated = async (req, res, next) => {
             })
         }
         const decode = await jwt.verify(token, process.env.SECRET_KEY);
+        console.log("Decoded token:", decode);
         if(!decode){
             return res.status(401).json({
                 message:"Invalid token",
